59d2915650e030a32e5e986f0326692b0b0dc734
Manuja Demin <156160654+Manuja-D@users.noreply.github.com>
2024-11-03 19:25:32 +0530

    Working 100% stack algorithm ^-^

--- cprogramme.c
+++ cprogramme.c
@@ -9,35 +9,42 @@ struct stackalgo{
 
 
 void push(int n, int array[], int len){
-    a.top++;
-    array[a.top] = n;
-    printf("%d is added :) \n \n " , n);
-
+    if(a.top >= len -1){
+        printf("Stack overflow cannot add more \n");
+    }else{
+        a.top++;
+        array[a.top] = n;
+        printf("%d is added :) \n \n " , n);
+    }
 }
 
 
 int pop(int arr[],int len){
-    int output = arr[a.top];
-    a.top -- ;
-    return output ;
-
+    if(a.top < 0){
+        return -1;
+    }else{
+        int output = arr[a.top];
+        a.top -- ;
+        return output ;
+    }
 }
 
 void display(int arr[],int len){
-    for(int i = 0; i <len ; i++){
-        printf("%d , " , arr[i]);
-    }
-}
-
-int isfull(int len){
-    if(a.top == len){
-        return true;
+    if(a.top < 0){
+        printf("Empty!");
     }else{
-        return false;
+        for(int i = 0; i <len ; i++){
+            printf("%d , " , arr[i]);
+
+        }
     }
+
 }
 
+bool isfull(int len){
+    return a.top == len -1;
 
+}
 
 
 int main(){
@@ -53,34 +60,42 @@ int main(){
 
     int array[len];
 
-    bool func = isfull(len);
+    bool func = false;
 
     do{
-
         printf("If you want to add an item press 1 : \n");
         printf("If you want to remove the lastly added item press 2 :\n");
+        printf("Press 3 to display all the values...");
 
         int ans;
         scanf("%d",&ans);
 
+
         if(ans == 1){
             int val;
+
             printf("Enter a value to add");
+
             scanf("%d" , &val);
             push(val,array,len);
             func = isfull(len);
 
-        }else{
+        }else if(ans == 2){
+
             printf("%d was removed :(  \n", pop(array,len));
+
+        }else if(ans == 3){
+            display(array,len);
+
+        }else{
+            printf("Invalid input(should only contain integer values -_- )");
+
         }
 
-    }while(func);
-
+    }while(!func);
 
     display(array,len);
 
     return 0;
 
 }
-
-
